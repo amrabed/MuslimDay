4445a6ad39baf97c0beddaecf3e98fc959d68740
Amr Abed <amraabed@gmail.com>
2017-01-12 13:33:56 -0500

    Separate navigation drawer code

--- app/src/main/java/amrabed/android/release/evaluation/MainActivity.java
+++ app/src/main/java/amrabed/android/release/evaluation/MainActivity.java
@@ -3,8 +3,6 @@ package amrabed.android.release.evaluation;
 import android.Manifest;
 import android.accounts.AccountManager;
 import android.app.AlertDialog;
-import android.app.Fragment;
-import android.app.backup.BackupManager;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender;
@@ -14,21 +12,15 @@ import android.os.PersistableBundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.NavigationView;
 import android.support.v13.app.ActivityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 
-import amrabed.android.release.evaluation.api.ApiManager;
 import amrabed.android.release.evaluation.api.SyncTask;
 import amrabed.android.release.evaluation.app.ApplicationEvaluation;
 import amrabed.android.release.evaluation.db.Database;
@@ -40,137 +32,122 @@ import amrabed.android.release.evaluation.db.DatabaseUpdater;
  *
  * @author AmrAbed
  */
-public class MainActivity extends AppCompatActivity implements SyncTask.Listener,
-		ApiManager.Listener, NavigationView.OnNavigationItemSelectedListener
+public class MainActivity extends BaseActivity
 {
-	private static final String TAG = MainActivity.class.getName();
+    private static final String TAG = MainActivity.class.getName();
 
-	private static final int READ_CONTACTS_PERMISSION_REQUEST = 0;
-	private static final int RESOLVE_CONNECTION_REQUEST = 5;
-	private static final int ACCOUNT_PICKER_REQUEST = 1;
-	private static final int COMPLETE_AUTHORIZATION_REQUEST = 4;
+    private static final int READ_CONTACTS_PERMISSION_REQUEST = 0;
+    private static final int RESOLVE_CONNECTION_REQUEST = 5;
+    private static final int ACCOUNT_PICKER_REQUEST = 1;
+    private static final int COMPLETE_AUTHORIZATION_REQUEST = 4;
 
-	private static final String INDEX_KEY = "Navigation index key";
+    private NavigationDrawer drawer;
 
-	private int navigationIndex;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
 
-	private ApiManager apiManager;
+        if (isSyncEnabled())
+        {
+            getApiManager();
+        }
 
-	private BackupManager backupManager;
-	private Toolbar toolbar;
-	private DrawerLayout drawer;
-	private NavigationView navigationView;
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main_activity);
+        drawer = new NavigationDrawer(this).create(savedInstanceState, toolbar);
 
-		if (isSyncEnabled())
-		{
-			apiManager = new ApiManager(this, this);
-//			getBackupManager().requestRestore(new RestoreObserver(){});
-		}
+        Database db = ApplicationEvaluation.getDatabase();
+        db.insert(new DatabaseEntry(DatabaseUpdater.today.getMillis(), 0));
+    }
 
-		toolbar = (Toolbar) findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        if (isSyncEnabled())
+        {
+            sync();
+        }
+        getBackupManager().dataChanged();
+    }
 
-		Database db = ApplicationEvaluation.getDatabase();
-		db.insert(new DatabaseEntry(DatabaseUpdater.today.getMillis(), 0));
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState)
+    {
+        super.onSaveInstanceState(outState, outPersistentState);
+        drawer.saveState(outState);
+    }
 
-		if (savedInstanceState != null)
-		{
-			navigationIndex = savedInstanceState.getInt(INDEX_KEY);
-		}
-		setUpNavigationDrawer();
-	}
+    @Override
+    public void onBackPressed()
+    {
+        if (drawer.isOpen())
+        {
+            drawer.close();
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+    }
 
-	@Override
-	protected void onStop()
-	{
-		if(isSyncEnabled())
-		{
-			sync();
-		}
-		getBackupManager().dataChanged();
-		super.onStop();
-	}
-
-	@Override
-	public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState)
-	{
-		super.onSaveInstanceState(outState, outPersistentState);
-		outState.putInt(INDEX_KEY, navigationIndex);
-	}
-
-	@Override
-	public void onBackPressed()
-	{
-		if (drawer.isDrawerOpen(navigationView))
-		{
-			drawer.closeDrawers();
-		}
-		else
-		{
-			super.onBackPressed();
-		}
-	}
-
-	@Override
-	public void onActivityResult(final int requestCode, final int resultCode, final Intent data)
-	{
-		// ToDo: Fix error messages
-		switch (requestCode)
-		{
-			case READ_CONTACTS_PERMISSION_REQUEST:
-				if (resultCode == RESULT_OK)
-				{
-					pickAccount();
-				}
-				else
-				{
-					Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show();
-				}
-				break;
-			case ACCOUNT_PICKER_REQUEST:
-				if (resultCode == RESULT_OK && data != null && data.getExtras() != null)
-				{
-					final String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
-					if (!TextUtils.isEmpty(accountName))
-					{
-						PreferenceManager.getDefaultSharedPreferences(this).edit()
-								.putString(AccountManager.KEY_ACCOUNT_NAME, accountName).apply();
-						apiManager.getCredential().setSelectedAccountName(accountName);
-					}
-					sync();
-				}
-				else
-				{
-					Toast.makeText(this, "Sync not authorized", Toast.LENGTH_LONG).show();
-				}
-				break;
-			case RESOLVE_CONNECTION_REQUEST:
-				if (resultCode == RESULT_OK)
-				{
-					apiManager.getClient().connect();
-				}
-				else
-				{
-					Toast.makeText(this, "Connection Error", Toast.LENGTH_LONG).show();
-				}
-				break;
-			case COMPLETE_AUTHORIZATION_REQUEST:
-				if (resultCode == RESULT_OK)
-				{
-					// App is authorized, you can go back to sending the API request
-					sync();
-				}
-				else
-				{
-					pickAccount();
-				}
-				break;
+    @Override
+    public void onActivityResult(final int requestCode, final int resultCode, final Intent data)
+    {
+        // ToDo: Fix error messages
+        switch (requestCode)
+        {
+            case READ_CONTACTS_PERMISSION_REQUEST:
+                if (resultCode == RESULT_OK)
+                {
+                    pickAccount();
+                }
+                else
+                {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case ACCOUNT_PICKER_REQUEST:
+                if (resultCode == RESULT_OK && data != null && data.getExtras() != null)
+                {
+                    final String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                    if (!TextUtils.isEmpty(accountName))
+                    {
+                        PreferenceManager.getDefaultSharedPreferences(this).edit()
+                                .putString(AccountManager.KEY_ACCOUNT_NAME, accountName).apply();
+                        getApiManager().getCredential().setSelectedAccountName(accountName);
+                    }
+                    sync();
+                }
+                else
+                {
+                    Toast.makeText(this, "Sync not authorized", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case RESOLVE_CONNECTION_REQUEST:
+                if (resultCode == RESULT_OK)
+                {
+                    getApiManager().getClient().connect();
+                }
+                else
+                {
+                    Toast.makeText(this, "Connection Error", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case COMPLETE_AUTHORIZATION_REQUEST:
+                if (resultCode == RESULT_OK)
+                {
+                    // App is authorized, you can go back to sending the API request
+                    sync();
+                }
+                else
+                {
+                    pickAccount();
+                }
+                break;
 //			case REQUEST_AUTHORIZATION:
 //			case REQUEST_AUTHORIZATION_SYNC:
 //				if (resultCode == RESULT_OK)
@@ -190,270 +167,145 @@ public class MainActivity extends AppCompatActivity implements SyncTask.Listener
 //							ACCOUNT_PICKER_REQUEST);
 //				}
 //				break;
-		}
-	}
+        }
+    }
 
-	@Override
-	public void onConnected(@Nullable Bundle bundle)
-	{
-		Log.i(TAG, "Connection to Google API client completed successfully");
-		checkPermissions();
-	}
+    @Override
+    public void onConnected(@Nullable Bundle bundle)
+    {
+        Log.i(TAG, "Connection to Google API client completed successfully");
+        checkPermissions();
+    }
 
-	@Override
-	public void onConnectionSuspended(int i)
-	{
-		Log.w(TAG, "Connection to Google API suspended");
-		apiManager.connect();
-	}
+    @Override
+    public void onConnectionSuspended(int i)
+    {
+        Log.w(TAG, "Connection to Google API suspended");
+        getApiManager().connect();
+    }
 
-	@Override
-	public void onConnectionFailed(@NonNull ConnectionResult result)
-	{
-		Log.e(TAG, "Connection to Google API client failed - Error: " + result.getErrorMessage());
-		if (result.hasResolution())
-		{
-			try
-			{
-				result.startResolutionForResult(this, RESOLVE_CONNECTION_REQUEST);
-			}
-			catch (IntentSender.SendIntentException e)
-			{
-				Log.wtf(TAG, e);
-				Toast.makeText(this, "Connection failed", Toast.LENGTH_LONG).show();
-			}
-		}
-		else
-		{
-			GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), this, 0).show();
-		}
-	}
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult result)
+    {
+        Log.e(TAG, "Connection to Google API client failed - Error " + result.getErrorCode());
+        if (result.hasResolution())
+        {
+            try
+            {
+                result.startResolutionForResult(this, RESOLVE_CONNECTION_REQUEST);
+            }
+            catch (IntentSender.SendIntentException e)
+            {
+                Log.wtf(TAG, e);
+                Toast.makeText(this, "Connection failed", Toast.LENGTH_LONG).show();
+            }
+        }
+        else
+        {
+            GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), this, 0).show();
+        }
+    }
 
-	@Override
-	public boolean onNavigationItemSelected(@NonNull MenuItem item)
-	{
-		switch (item.getItemId())
-		{
-			case R.id.nav_eval:
-				navigationIndex = 0;
-				setTitle(R.string.evaluation);
-				getFragmentManager().beginTransaction()//.addToBackStack(null)
-						.replace(R.id.content, new EvaluationSection()).commit();
-				break;
-			case R.id.nav_progress:
-				navigationIndex = 1;
-				setTitle(R.string.menu_progress);
-				getFragmentManager().beginTransaction()//.addToBackStack(null)
-						.replace(R.id.content, new WeeklyProgressFragment()).commit();
-				break;
-			case R.id.nav_guide:
-				navigationIndex = 2;
-				setTitle(R.string.menu_progress);
-				getFragmentManager().beginTransaction()//.addToBackStack(null)
-						.replace(R.id.content, new GuideSection()).commit();
-				break;
-			case R.id.nav_preferences:
-				navigationIndex = 3;
-				getFragmentManager().beginTransaction()//.addToBackStack(null)
-						.replace(R.id.content, new PreferenceSection()).commit();
-				break;
-			case R.id.nav_edit:
-				navigationIndex = 4;
-				getFragmentManager().beginTransaction()//.addToBackStack(null)
-						.replace(R.id.content, new EditSection()).commit();
-				break;
-			case R.id.nav_Settings:
-				navigationIndex = 5;
-				getFragmentManager().beginTransaction()//.addToBackStack(null)
-						.replace(R.id.content, new SettingsSection()).commit();
-				break;
-			case R.id.nav_help:
-				new AlertDialog.Builder(this)
-						.setTitle("Help")
-						.setView(getLayoutInflater().inflate(R.layout.help_dialog, null))
-						.create().show();
-				break;
-			case R.id.nav_about:
-				new AlertDialog.Builder(this)
-						.setTitle(R.string.menu_about)
-						.setMessage(R.string.about_content1)
-						.create().show();
-				break;
-		}
-		invalidateOptionsMenu();
-		drawer.closeDrawers();
-		return true;
-	}
+    @Override
+    public void onSyncSuccess(boolean isSaved, boolean isUpdated)
+    {
+        if ((!isUpdated) && (!isSaved))
+        {
+            Toast.makeText(this, R.string.no_change, Toast.LENGTH_SHORT).show();
+        }
+        else if (isUpdated)
+        {
+            Toast.makeText(this, R.string.updating, Toast.LENGTH_SHORT).show();
+            restart(false);
+        }
+        else // if (isSaved)
+        {
+            Toast.makeText(this, R.string.saved, Toast.LENGTH_SHORT).show();
+        }
+    }
 
-	@Override
-	public void onSyncSuccess(boolean isSaved, boolean isUpdated)
-	{
-		if ((!isUpdated) && (!isSaved))
-		{
-			Toast.makeText(this, R.string.no_change, Toast.LENGTH_SHORT).show();
-		}
-		else if (isUpdated)
-		{
-			Toast.makeText(this, R.string.updating, Toast.LENGTH_SHORT).show();
-			restart(false);
-		}
-		else // if (isSaved)
-		{
-			Toast.makeText(this, R.string.saved, Toast.LENGTH_SHORT).show();
-		}
-	}
+    @Override
+    public void onSyncError(Exception e)
+    {
+        Toast.makeText(this, R.string.sync_error, Toast.LENGTH_LONG).show();
+    }
 
-	@Override
-	public void onSyncError(Exception e)
-	{
-		Toast.makeText(this, R.string.sync_error, Toast.LENGTH_LONG).show();
-	}
+    private void checkPermissions()
+    {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_CONTACTS},
+                    READ_CONTACTS_PERMISSION_REQUEST);
+        }
+        else
+        {
+            pickAccount();
+        }
+    }
 
-	private void checkPermissions()
-	{
-		if (ActivityCompat.checkSelfPermission(this,
-				Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED)
-		{
-			ActivityCompat.requestPermissions(this,
-					new String[]{Manifest.permission.READ_CONTACTS},
-					READ_CONTACTS_PERMISSION_REQUEST);
-		}
-		else
-		{
-			pickAccount();
-		}
-	}
-
-	private void pickAccount()
-	{
-		try
-		{
+    private void pickAccount()
+    {
+        try
+        {
 
 //			final String account = apiManager.getCredential().getSelectedAccountName();
-			final String account = PreferenceManager.getDefaultSharedPreferences(this)
-					.getString(AccountManager.KEY_ACCOUNT_NAME, null);
-			if (TextUtils.isEmpty(account))
-			{
-				startActivityForResult(apiManager.getCredential().newChooseAccountIntent(),
-						ACCOUNT_PICKER_REQUEST);
-			}
-			else
-			{
-				sync();
-			}
-		}
-		catch (Exception e)
-		{
-			Toast.makeText(getBaseContext(), "Error: Have you set your Google account on device?",
-					Toast.LENGTH_LONG).show();
-			Log.e(TAG, e.toString());
-		}
-	}
+            final String account = PreferenceManager.getDefaultSharedPreferences(this)
+                    .getString(AccountManager.KEY_ACCOUNT_NAME, null);
+            if (TextUtils.isEmpty(account))
+            {
+                startActivityForResult(getApiManager().getCredential().newChooseAccountIntent(),
+                        ACCOUNT_PICKER_REQUEST);
+            }
+            else
+            {
+                sync();
+            }
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(getBaseContext(), "Error: Have you set your Google account on device?",
+                    Toast.LENGTH_LONG).show();
+            Log.e(TAG, e.toString());
+        }
+    }
 
-	private void sync()
-	{
-		apiManager.getDriveService();
-		new SyncTask(this, this).execute(apiManager.getClient());
-	}
-//	private GoogleAccountCredential getCredential()
-//	{
-//		if (credential == null)
-//		{
-//			credential = apiManager.getCredential();
-//		}
-//		return credential;
-//	}
+    private void sync()
+    {
+        getApiManager().getDriveService();
+        new SyncTask(this, this).execute(getApiManager().getClient());
+    }
 
-	private void setUpNavigationDrawer()
-	{
-		loadCurrentFragment();
-
-		drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-		navigationView = (NavigationView) findViewById(R.id.navigation);
-		navigationView.setNavigationItemSelectedListener(this);
-		navigationView.getMenu().getItem(navigationIndex).setChecked(true);
-
-//		entries = ApplicationEvaluation.getDatabase().getAllEntries();
+    public void handleSyncRequest()
+    {
+        checkPermissions();
+    }
 
 
-		final ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,
-				toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+    void restart(boolean shouldShowDialog)
+    {
+        if (!shouldShowDialog)
+        {
+            finish();
+            android.os.Process.killProcess(android.os.Process.myPid());
+            startActivity(new Intent(MainActivity.this, MainActivity.class));
+            return;
+        }
+        // Show confirmation dialog to restart app, so user can see what's going on
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.restart)
+                .setCancelable(true)
+                .setPositiveButton(R.string.res_yes, new DialogInterface.OnClickListener()
+                {
 
-		drawer.addDrawerListener(toggle);
-		toggle.syncState();
-	}
-
-	private void loadCurrentFragment()
-	{
-		Fragment fragment;
-		switch (navigationIndex)
-		{
-			case 5:
-				fragment = new SettingsSection();
-				break;
-			case 4:
-				fragment = new EditSection();
-				break;
-			case 3:
-				fragment = new PreferenceSection();
-				break;
-			case 2:
-				fragment = new GuideSection();
-				break;
-			case 1:
-				fragment = new PreferenceSection();
-				break;
-			case 0:
-			default:
-				fragment = new EvaluationSection();
-		}
-		getFragmentManager().beginTransaction().replace(R.id.content, fragment).commit();
-	}
-
-	public void handleSyncRequest()
-	{
-		checkPermissions();
-	}
-
-
-	void restart(boolean shouldShowDialog)
-	{
-		if (!shouldShowDialog)
-		{
-			finish();
-			android.os.Process.killProcess(android.os.Process.myPid());
-			startActivity(new Intent(MainActivity.this, MainActivity.class));
-			return;
-		}
-		// Show confirmation dialog to restart app, so user can see what's going on
-		new AlertDialog.Builder(this)
-				.setMessage(R.string.restart)
-				.setCancelable(true)
-				.setPositiveButton(R.string.res_yes, new DialogInterface.OnClickListener()
-				{
-
-					@Override
-					public void onClick(DialogInterface dialog, int which)
-					{
-						// Restart Application
-						restart(false);
-					}
-				})
-				.create().show();
-	}
-
-	public boolean isSyncEnabled()
-	{
-		return PreferenceManager.getDefaultSharedPreferences(this).getBoolean("sync", false);
-	}
-
-	private BackupManager getBackupManager()
-	{
-		if(backupManager == null)
-		{
-			backupManager = new BackupManager(this);
-		}
-		return backupManager;
-	}
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        // Restart Application
+                        restart(false);
+                    }
+                })
+                .create().show();
+    }
 }
---
+++ app/src/main/java/amrabed/android/release/evaluation/NavigationDrawer.java
@@ -0,0 +1,139 @@
+package amrabed.android.release.evaluation;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+/**
+ * Navigation Drawer
+ *
+ * @author AmrAbed
+ */
+
+public class NavigationDrawer implements NavigationView.OnNavigationItemSelectedListener
+{
+    private static final String INDEX_KEY = "Navigation index key";
+
+    private final Activity activity;
+
+    private int currentIndex;
+
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+
+    public NavigationDrawer(Activity activity)
+    {
+        this.activity = activity;
+    }
+
+    public NavigationDrawer create(Bundle savedInstanceState, Toolbar toolbar)
+    {
+        if (savedInstanceState != null)
+        {
+            currentIndex = savedInstanceState.getInt(INDEX_KEY);
+        }
+
+        loadFragment(currentIndex);
+
+        drawer = (DrawerLayout) activity.findViewById(R.id.drawer_layout);
+
+        navigationView = (NavigationView) activity.findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.getMenu().getItem(currentIndex).setChecked(true);
+
+        final ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(activity, drawer,
+                toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        return this;
+    }
+
+    public boolean isOpen()
+    {
+        return drawer.isDrawerOpen(navigationView);
+    }
+
+    public void saveState(Bundle outState)
+    {
+        outState.putInt(INDEX_KEY, currentIndex);
+    }
+
+    public void close()
+    {
+        drawer.closeDrawers();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.nav_eval:
+            case R.id.nav_progress:
+            case R.id.nav_guide:
+            case R.id.nav_preferences:
+            case R.id.nav_edit:
+            case R.id.nav_Settings:
+                loadFragment(item.getItemId());
+                break;
+            case R.id.nav_help:
+                new android.app.AlertDialog.Builder(activity)
+                        .setTitle("Help")
+                        .setView(activity.getLayoutInflater().inflate(R.layout.help_dialog, null))
+                        .create().show();
+                break;
+            case R.id.nav_about:
+                new android.app.AlertDialog.Builder(activity)
+                        .setTitle(R.string.menu_about)
+                        .setMessage(R.string.about_content1)
+                        .create().show();
+                break;
+        }
+        activity.invalidateOptionsMenu();
+        close();
+        return true;
+    }
+
+    private void loadFragment(int id)
+    {
+        currentIndex = id;
+        Fragment fragment;
+        switch (currentIndex)
+        {
+            case R.id.nav_Settings:
+                activity.setTitle(R.string.menu_settings);
+                fragment = new SettingsSection();
+                break;
+            case R.id.nav_edit:
+                activity.setTitle(R.string.menu_edit);
+                fragment = new EditSection();
+                break;
+            case R.id.nav_progress:
+                activity.setTitle(R.string.menu_progress);
+                fragment = new WeeklyProgressFragment();
+                break;
+            case R.id.nav_guide:
+                activity.setTitle(R.string.menu_guide);
+                fragment = new GuideSection();
+                break;
+            case R.id.nav_preferences:
+                activity.setTitle(R.string.menu_preferences);
+                fragment = new PreferenceSection();
+                break;
+            case R.id.nav_eval:
+            default:
+                activity.setTitle(R.string.evaluation);
+                fragment = new EvaluationSection();
+        }
+        activity.getFragmentManager().beginTransaction()//.addToBackStack(null)
+                .replace(R.id.content, fragment).commit();
+    }
+}
