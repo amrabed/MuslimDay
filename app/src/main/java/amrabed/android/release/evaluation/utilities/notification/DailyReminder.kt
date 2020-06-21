d8343707b8af34e93a210e2a4ad43e5085ef54ed
Amr Abed <amraabed@gmail.com>
2020-06-21 19:19:12 -0400

    Fix reminder logic

--- app/src/main/java/amrabed/android/release/evaluation/utilities/notification/DailyReminder.kt
+++ app/src/main/java/amrabed/android/release/evaluation/utilities/notification/DailyReminder.kt
@@ -4,7 +4,6 @@ import amrabed.android.release.evaluation.R
 import amrabed.android.release.evaluation.utilities.locale.LocaleManager
 import android.app.AlarmManager
 import android.app.PendingIntent
-import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
@@ -13,48 +12,48 @@ import org.joda.time.LocalTime
 /**
  * Utility to schedule, cancel, and show daily reminder
  */
-object DailyReminder : BaseReminder {
-    private val NAME = DailyReminder::class.java.name
+class DailyReminder : BaseReminder() {
+    override fun onReceive(context: Context, intent: Intent?) {
+        Log.i(NAME, "Handling notification for intent: $intent")
+        LocaleManager.setLocale(context.applicationContext)
+        showNotification(context)
+    }
 
-    fun toggle(context: Context, isEnabled: Boolean) {
-        if (isEnabled) {
-            schedule(context)
-        } else {
-            cancel(context)
+    companion object {
+        private val NAME = DailyReminder::class.java.name
+
+        fun toggle(context: Context, isEnabled: Boolean) {
+            if (isEnabled) {
+                schedule(context)
+            } else {
+                cancel(context)
+            }
         }
-    }
 
-    private fun schedule(context: Context) {
-        Log.i(NAME, "Scheduling daily reminder")
-        val intent = PendingIntent.getBroadcast(context, 0, Intent(context, Notifier::class.java), PendingIntent.FLAG_UPDATE_CURRENT)
-        getAlarmManager(context).setInexactRepeating(AlarmManager.RTC,
-                LocalTime(19, 0).toDateTimeToday().millis,
-                AlarmManager.INTERVAL_DAY, intent)
-    }
+        private fun schedule(context: Context) {
+            Log.i(NAME, "Scheduling daily reminder")
+            val intent = PendingIntent.getBroadcast(context, 0, Intent(context, DailyReminder::class.java), PendingIntent.FLAG_UPDATE_CURRENT)
+            getAlarmManager(context).setInexactRepeating(AlarmManager.RTC,
+                    LocalTime(19, 0).toDateTimeToday().millis,
+                    AlarmManager.INTERVAL_DAY, intent)
+        }
 
-    private fun cancel(context: Context) {
-        Log.i(NAME, "Cancelling notification")
-        val intent = PendingIntent.getBroadcast(context, 0, Intent(context, Notifier::class.java), PendingIntent.FLAG_NO_CREATE)
-        if (intent != null) getAlarmManager(context).cancel(intent)
-    }
+        private fun cancel(context: Context) {
+            Log.i(NAME, "Cancelling notification")
+            val intent = PendingIntent.getBroadcast(context, 0, Intent(context, DailyReminder::class.java), PendingIntent.FLAG_NO_CREATE)
+            if (intent != null) getAlarmManager(context).cancel(intent)
+        }
 
-    private fun showNotification(context: Context) {
-        Log.i(NAME, "Showing daily reminder notification")
-        val channelId = "Daily Reminder Channel"
-        buildNotificationChannel(context, channelId)
+        private fun showNotification(context: Context) {
+            Log.i(NAME, "Showing daily reminder notification")
+            val channelId = "Daily Reminder Channel"
+            buildNotificationChannel(context, channelId)
 
-        val notification = getNotificationBuilder(context, channelId)
-                .setContentTitle(context.getString(R.string.appName))
-                .setContentText(context.getString(R.string.notificationContent))
-                .build()
-        getNotificationManager(context).notify(0, notification)
-    }
-
-    class Notifier : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent?) {
-            Log.i(NAME, "Handling notification for intent: $intent")
-            LocaleManager.setLocale(context.applicationContext)
-            showNotification(context)
+            val notification = getNotificationBuilder(context, channelId)
+                    .setContentTitle(context.getString(R.string.appName))
+                    .setContentText(context.getString(R.string.notificationContent))
+                    .build()
+            getNotificationManager(context).notify(0, notification)
         }
     }
 }
--- app/src/main/java/amrabed/android/release/evaluation/utilities/notification/TaskReminder.kt
+++ app/src/main/java/amrabed/android/release/evaluation/utilities/notification/TaskReminder.kt
@@ -20,56 +20,58 @@ import org.joda.time.LocalDateTime
 /**
  * Utility to schedule, cancel, and show task reminders
  */
-object TaskReminder : BaseReminder {
-    private val NAME = TaskReminder::class.java.name
-
-    fun schedule(context: Context, task: Task?) {
+class TaskReminder : BaseReminder() {
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.i(NAME, "Handling notification for intent: $intent")
+        LocaleManager.setLocale(context.applicationContext)
+        val task = intent.extras?.getBundle(NAME)?.getParcelable(TASK) as Task?
         if (task?.reminder != null) {
-            Log.i(NAME, "Scheduling notification for task ${task.id} at ${task.reminder}")
-            val intent = Intent(context, Notifier::class.java).putExtra(NAME, bundleOf(Pair(TASK, task)))
-            val pendingIntent = PendingIntent.getBroadcast(context, task.id.hashCode(), intent, PendingIntent.FLAG_CANCEL_CURRENT)
-            getAlarmManager(context).set(AlarmManager.RTC, DateTime.parse(task.reminder).millis, pendingIntent)
+            showNotification(context, task)
+            schedule(context, task.nextReminder(LocalDateTime.parse(task.reminder).toLocalTime()))
+            Repository(context).addTask(task)
         }
     }
 
-    fun cancel(context: Context, task: Task) {
-        Log.i(NAME, "Cancelling notification for task: ${task.id}")
-        val intent = PendingIntent.getBroadcast(context, task.id.hashCode(), Intent(context, Notifier::class.java), PendingIntent.FLAG_NO_CREATE)
-        if (intent != null) getAlarmManager(context).cancel(intent)
-    }
+    companion object {
+        private val NAME = TaskReminder::class.java.name
 
-    fun showNotification(context: Context, task: Task?) {
-        Log.i(NAME, "Showing notification for task: ${task?.id}")
-        val channelId = "Task Reminder Channel"
-        buildNotificationChannel(context, channelId)
-
-        val actionIntents = Status.values().map {
-            PendingIntent.getBroadcast(context, task?.id.hashCode(),
-                    Intent(context, ActionHandler::class.java).setAction(it.name).putExtra(NAME, task?.id),
-                    PendingIntent.FLAG_CANCEL_CURRENT)
-        }
-
-        val notification = getNotificationBuilder(context, channelId)
-                .setGroup(NAME)
-                .setContentTitle(task?.getTitle(context))
-                .setContentText(context.getString(R.string.reminderNotificationText))
-                .addAction(Status.NONE.icon, context.getString(Status.NONE.title), actionIntents[Status.NONE.ordinal])
-                .addAction(Status.MISSED.icon, context.getString(Status.MISSED.title), actionIntents[Status.MISSED.ordinal])
-                .addAction(Status.DONE.icon, context.getString(Status.DONE.title), actionIntents[Status.DONE.ordinal])
-                .build()
-
-        getNotificationManager(context).notify(task?.id.hashCode(), notification)
-    }
-
-    class Notifier : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            Log.i(NAME, "Handling notification for intent: $intent")
-            LocaleManager.setLocale(context.applicationContext)
-            val task = intent.extras?.getBundle(NAME)?.getParcelable(TASK) as Task?
+        fun schedule(context: Context, task: Task?) {
             if (task?.reminder != null) {
-                showNotification(context, task)
-                schedule(context, task.nextReminder(LocalDateTime.parse(task.reminder).toLocalTime()))
+                Log.i(NAME, "Scheduling notification for task ${task.id} at ${task.reminder}")
+                val intent = Intent(context, TaskReminder::class.java).putExtra(NAME, bundleOf(Pair(TASK, task)))
+                val pendingIntent = PendingIntent.getBroadcast(context, task.id.hashCode(), intent, PendingIntent.FLAG_CANCEL_CURRENT)
+                getAlarmManager(context).set(AlarmManager.RTC, DateTime.parse(task.reminder).millis, pendingIntent)
             }
+        }
+
+        fun cancel(context: Context, task: Task) {
+            Log.i(NAME, "Cancelling notification for task: ${task.id}")
+            val intent = PendingIntent.getBroadcast(context, task.id.hashCode(), Intent(context, TaskReminder::class.java), PendingIntent.FLAG_NO_CREATE)
+            if (intent != null) getAlarmManager(context).cancel(intent)
+        }
+
+        fun showNotification(context: Context, task: Task?) {
+            Log.i(NAME, "Showing notification for task: ${task?.id}")
+            val channelId = "Task Reminder Channel"
+            buildNotificationChannel(context, channelId)
+
+            val actionIntents = Status.values().map {
+                PendingIntent.getBroadcast(context, task?.id.hashCode(),
+                        Intent(context, ActionHandler::class.java).setAction(it.name).putExtra(NAME, task?.id),
+                        PendingIntent.FLAG_CANCEL_CURRENT)
+            }
+
+            val notification = getNotificationBuilder(context, channelId)
+                    .setGroup(NAME)
+                    .setContentTitle(task?.getTitle(context))
+                    .setContentText(context.getString(R.string.reminderNotificationText))
+                    .setContentInfo(DateTime.parse(task?.reminder).toLocalTime().toString("h:mm a"))
+                    .addAction(Status.NONE.icon, context.getString(Status.NONE.title), actionIntents[Status.NONE.ordinal])
+                    .addAction(Status.MISSED.icon, context.getString(Status.MISSED.title), actionIntents[Status.MISSED.ordinal])
+                    .addAction(Status.DONE.icon, context.getString(Status.DONE.title), actionIntents[Status.DONE.ordinal])
+                    .build()
+
+            getNotificationManager(context).notify(task?.id.hashCode(), notification)
         }
     }
 
